b61646c9492c265abad035e6fec0bec4c4cd52ea
Olha Tkachuk <2006olgaolga2006@ukr.net>
2022-12-24 14:28:50 +0100

    Init commit

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ lista_zakupow = {
     }
 total = 0
 print('Список покупок')
+lista_zakupow = {
+    "пекарня":['Хліб', 'Пончик', 'Булки'],
+    "продуктовий": ['Морква', 'Селера', 'Рукола']
+    }
+total = 0
+print('Список покупок')
+for shop, product in lista_zakupow.items():
+    print(f"Я йду до {shop.capitalize()} і куплю там {product}")
+    total = total + len(product)
+print(f"Разом купую {total} товарів.")
+
